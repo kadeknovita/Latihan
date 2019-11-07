8abf1dce1c4abf5d1a805a39aefe0834fae5af29
kadeknovita <novitaprahastha@gmail.com>
2019-11-07 10:18:00 +0700

    storage folder

--- tes1.py
+++ tes1.py
@@ -1,1 +1,12 @@
 print('welcome')
+x = 12
+print(x+x)
+
+# staging area => add . / nama file nya 
+# commit => commit
+#untuk hapus yang ga jadi di staging area : git rm --cached nama file
+#cek ada berapa commit: git log (semua nya)
+                        # git log --oneline (keluar intinya)
+#buat commit : git commit -m "komentar/keterangan"
+
+print(x/2)
